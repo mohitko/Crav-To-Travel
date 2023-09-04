2aaaeaadec474de30ab9c7ef16365efd8dd83c2c
mohitko <mohitkodape900@gmail.com>
2023-09-04 12:41:31 +0530

    Master page lineup

---
+++ script.js
@@ -0,0 +1,8 @@
+// JavaScript for Login/Logout buttons
+document.getElementById("loginBtn").addEventListener("click", function () {
+  alert("Logged in!");
+});
+
+document.getElementById("logoutBtn").addEventListener("click", function () {
+  alert("Logged out!");
+});
